057289dceff145c7902174dad5cae9fd476f6ab3
Brandon Zarrella <zarrellab@gmail.com>
2017-07-31 15:31:10 -0600

    Create UI

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
-document.querySelector('#app').innerText = 'Hello World'
+document.querySelector('#eggs').onclick = () => console.log('eggs')
+document.querySelector('#spam').onclick = () => console.log('spam')
+
+document.querySelector('#eggCount').innerText = 'eggs'
+document.querySelector('#spamCount').innerText = 'spam'
